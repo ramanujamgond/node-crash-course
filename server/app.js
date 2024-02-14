ec40f3e5c0895fcdb031b74d64d4bce47421cdc0
Ramanujam Gond <ramanujamgond@gmail.com>
2024-02-14 13:27:28 +0530

    feat: impleted the crud operation

--- server/app.js
+++ server/app.js
@@ -20,6 +20,7 @@ app.set('view engine', 'ejs');
 
 // middleware & static files
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 
 // app.use((req, res, next) => {
 //     console.log('new request made:');
@@ -112,6 +113,41 @@ app.get('/blogs', async (req, res) => {
     }
 })
 
+app.post('/blogs', async (req, res) => {
+    const blog = new Blog(req.body);
+    // blog.save().then((result) => {
+    //     res.redirect('/blogs');
+    // }).catch((err) => {
+    //     console.log(err);
+    // })
+
+    try {
+        const result = await blog.save();
+        res.redirect('/blogs');
+    } catch (err) {
+        console.log(err);
+    }
+
+})
+
+app.get('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+    Blog.findById(id).then((result) => {
+        res.render('details', { blog: result, title: 'Blog Details' });
+    }).catch((err) => {
+        console.log(err)
+    })
+})
+
+app.delete('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+
+    Blog.findByIdAndDelete(id).then((result) => {
+        res.json({ redirect: '/blogs' })
+    }).catch((err) => {
+        console.log(err)
+    })
+})
 
 app.get('/blogs/create', (req, res) => {
     res.render('create', { title: 'Create a new Blog' });
